2b57001a0068f3e91af21fd865e44e712c2096de
amarsh020 <amoymarshalleck@gmail.com>
2024-07-15 23:04:37 -0400

    Update main.h

--- Amoy's Code/main.h
+++ Amoy's Code/main.h
@@ -10,3 +10,18 @@ typedef struct hash_struct
   uint32_t salary; //	32-bit unsigned integer (who wants a negative salary, eh?) to represent an annual salary.
   struct hash_struct *next; //pointer to the next node in the list
 } hashRecord;
+
+typedef struct thread{
+
+  char *command;
+  char *line2;
+  char *line3;
+} new_thread;
+
+char** getLineValues(char* line);
+
+void insert(FILE inputFile);
+void del(FILE inputFile);
+void search(FILE inputFile);
+void print(FILE inputFile);
+void* commandChooser(FILE *inputFile, void* vargp);
