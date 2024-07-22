663cfedc648acb7cecaac5b54c78ab85ddfb5d0a
cgordon03 <78285635+cgordon03@users.noreply.github.com>
2024-07-22 14:15:43 -0400

    Delete claytonscode directory

--- claytonscode/logging.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOGGING_H
-#define LOGGING_H
-
-void log_lock(const char *message, long long timestamp);
-void log_operation(const char *operation, const char *name, uint32_t salary);
-void log_search_result(const char *name, uint32_t salary);
-
-#endif // LOGGING_H
